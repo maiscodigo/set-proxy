57f65e4d50587e5343e623bfc894d894c9325d32
maiscodigo <41766661+maiscodigo@users.noreply.github.com>
2020-10-05 20:35:59 -0300

    Add as funções básicas para teste

--- FrmMain.Designer.vb
+++ FrmMain.Designer.vb
@@ -22,13 +22,48 @@ Partial Class FrmMain
     'Não o modifique usando o editor de códigos.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.LblInfo = New System.Windows.Forms.Label()
+        Me.LblStatus = New System.Windows.Forms.Label()
+        Me.BtnEstado = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'LblInfo
+        '
+        Me.LblInfo.AutoSize = True
+        Me.LblInfo.Font = New System.Drawing.Font("Calibri", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblInfo.Location = New System.Drawing.Point(12, 9)
+        Me.LblInfo.Name = "LblInfo"
+        Me.LblInfo.Size = New System.Drawing.Size(251, 36)
+        Me.LblInfo.TabIndex = 0
+        Me.LblInfo.Text = "Estado da conexão:"
+        '
+        'LblStatus
+        '
+        Me.LblStatus.AutoSize = True
+        Me.LblStatus.Font = New System.Drawing.Font("Calibri", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblStatus.ForeColor = System.Drawing.Color.Red
+        Me.LblStatus.Location = New System.Drawing.Point(254, 9)
+        Me.LblStatus.Name = "LblStatus"
+        Me.LblStatus.Size = New System.Drawing.Size(61, 36)
+        Me.LblStatus.TabIndex = 1
+        Me.LblStatus.Text = "OFF"
+        '
+        'BtnEstado
+        '
+        Me.BtnEstado.Location = New System.Drawing.Point(18, 48)
+        Me.BtnEstado.Name = "BtnEstado"
+        Me.BtnEstado.Size = New System.Drawing.Size(75, 23)
+        Me.BtnEstado.TabIndex = 2
+        Me.BtnEstado.UseVisualStyleBackColor = True
         '
         'FrmMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(448, 169)
+        Me.Controls.Add(Me.BtnEstado)
+        Me.Controls.Add(Me.LblStatus)
+        Me.Controls.Add(Me.LblInfo)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.MaximizeBox = False
         Me.Name = "FrmMain"
@@ -36,7 +71,11 @@ Partial Class FrmMain
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Main"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents LblInfo As Label
+    Friend WithEvents LblStatus As Label
+    Friend WithEvents BtnEstado As Button
 End Class
--- FrmMain.vb
+++ FrmMain.vb
@@ -1,3 +1,79 @@
-﻿Public Class FrmMain
+﻿Option Explicit On
 
+Imports Microsoft.Win32
+
+Public Class FrmMain
+    Const sHKCU As String = "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings"
+    Const sKeyProxy As String = "ProxyServer"
+    Const sKeyHabilitaProxy As String = "ProxyEnable"
+    Const sIpPorta As String = "seuip:porta"
+
+    Private Sub SubSetLabels(bStatus As Boolean)
+        If bStatus Then
+            LblStatus.Text = "ON"
+            LblStatus.ForeColor = Color.Green
+            BtnEstado.Text = "DESATIVAR"
+        Else
+            LblStatus.Text = "OFF"
+            LblStatus.ForeColor = Color.Red
+            BtnEstado.Text = "ATIVAR"
+        End If
+    End Sub
+
+    Private Sub SubSetProxyRegistro()
+        Try
+            'Setando ip do proxy
+            Registry.SetValue(sHKCU, sKeyProxy, sIpPorta, RegistryValueKind.String)
+
+            'Habilitando o proxy
+            Registry.SetValue(sHKCU, sKeyHabilitaProxy, "1", RegistryValueKind.String)
+
+            'Verifica se foi setado realmente
+            SubGetProxyRegistro()
+        Catch ex As Exception
+            Console.WriteLine(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub SubDesetProxyRegistro()
+        Try
+            'Setando ip do proxy como vazio
+            Registry.SetValue(sHKCU, sKeyProxy, "", RegistryValueKind.String)
+
+            'Desabilitando o proxy
+            Registry.SetValue(sHKCU, sKeyHabilitaProxy, "0", RegistryValueKind.String)
+
+            'Verifica se foi desetado realmente
+            SubGetProxyRegistro()
+        Catch ex As Exception
+            Console.WriteLine(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub SubGetProxyRegistro()
+        Try
+            'Recuperando o estado do proxy
+            Dim sValue As String = Registry.GetValue(sHKCU, sKeyHabilitaProxy, "0").ToString
+
+            If sValue = "0" Then
+                SubSetLabels(False)
+            Else
+                SubSetLabels(True)
+            End If
+        Catch ex As Exception
+            Console.WriteLine(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub BtnEstado_Click(sender As Object, e As EventArgs) Handles BtnEstado.Click
+        If BtnEstado.Text = "ATIVAR" Then
+            SubSetProxyRegistro()
+        Else
+            SubDesetProxyRegistro()
+        End If
+    End Sub
+
+    Private Sub FrmMain_Load(sender As Object, e As EventArgs) Handles Me.Load
+        SubGetProxyRegistro()
+    End Sub
 End Class
